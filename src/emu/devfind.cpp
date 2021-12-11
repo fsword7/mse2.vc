b0a14af195f444dc0b02e287972a2c5a176bddac
Timothy Stark <fsword007@gmail.com>
2021-12-10 22:09:14 -0500

    New device finder package but will be implemented later.

---
+++ src/emu/devfind.cpp
@@ -0,0 +1,7 @@
+// devfind.h - Device finder package
+//
+// Author:  Tim Stark
+// Date:    12/10/2021
+
+#include "emu/core.h"
+#include "emu/devfind.h"
---
+++ src/emu/devfind.h
@@ -0,0 +1,4 @@
+// devfind.h - Device finder package
+//
+// Author:  Tim Stark
+// Date:    12/10/2021
