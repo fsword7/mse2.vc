fcefaf3cea97603298e7b2f75a06fcfe527ec7f4
Timothy Stark <fsword007@gmail.com>
2022-01-05 11:37:24 -0500

    Now loading firmware images into region data space.

--- src/emu/fwloader.cpp
+++ src/emu/fwloader.cpp
@@ -13,7 +13,7 @@
 FirmwareLoader::FirmwareLoader(Machine &sys, UserConsole &user)
 : sysMachine(sys)
 {
-
+    processRegionList();
 }
 
 cfwEntry_t*FirmwareLoader::first(Device &dev)
@@ -31,9 +31,93 @@ cfwEntry_t *FirmwareLoader::next(cfwEntry_t *entry)
     return !FWENTRY_ISEND(entry) ? entry : nullptr;
 }
 
+emu::ioFile *FirmwareLoader::processImageFile(fs::path pathName, cfwEntry_t *entry)
+{
+    fs::path fwPath = "fw";
+    auto imageFile = new emu::ioFile(0);
+
+    fs::path fileName = fwPath / pathName / FW_GETNAME(entry);
+
+    if (!imageFile->open(fileName))
+    {
+        delete imageFile;
+        imageFile = nullptr;
+    }
+
+    return imageFile;
+}
+
+void FirmwareLoader::openImageFile(fs::path pkgName, cfwEntry_t *entry)
+{
+    imageFile = processImageFile(pkgName, entry);
+}
+
+void FirmwareLoader::closeImageFile()
+{
+    imageFile->close();
+    delete imageFile;
+    imageFile = nullptr;
+}
+
+int FirmwareLoader::readImageData(uint8_t *buffer, int length, cfwEntry_t *entry)
+{
+    int actual = 0;
+
+    if (imageFile != nullptr)
+        actual = imageFile->read(buffer, length);
+    return actual;
+}
+
+bool FirmwareLoader::loadImageData(cfwEntry_t *parent, cfwEntry_t *entry)
+{
+    int offset = FW_GETOFFSET(entry);
+    int length = FW_GETLENGTH(entry);
+    // int skip = FW_GETSKIP(entry);
+    // int dShift = FW_GETBITSHIFT(entry);
+    // int dMask = ((1 << FW_GETBITWIDTH(entry)));
+    // int gSize = FW_GETGROUPSIZE(entry);
+    // int reversed = FW_ISREVERSED(entry);
+    // int nGroups = (length + gSize - 1) / gSize;
+
+    uint8_t *base = region->getBase() + offset;
+    int actual;
+
+    fmt::printf("%s: Reading image data: off=%X len=%X\n", FW_GETNAME(entry), offset, length);
+    actual = readImageData(base, length, entry);
+    if (actual != length)
+    {
+        fmt::printf("%s: Got %d (%X) bytes - expected %d (%X) bytes\n",
+            FW_GETNAME(entry), actual, actual, length, length);
+        return false;
+    }
+    return true;
+}
+
 void FirmwareLoader::processImageEntries(ctag_t *pkgName, cfwEntry_t *&entry, const Device &dev)
 {
+    cfwEntry_t *parent = entry++;
 
+    while (!FWENTRY_ISREGIONEND(entry))
+    {
+        if (FWENTRY_ISFILE(entry))
+        {
+            cfwEntry_t *base = entry;
+            int imageLength = 0;
+
+            fmt::printf("%s: Loading image file '%s'...\n",
+                dev.getDeviceName(), FW_GETNAME(entry));
+            openImageFile(pkgName, entry);
+
+            if (imageFile != nullptr)
+            {
+                loadImageData(parent, entry);
+                closeImageFile();
+            }
+            entry++;
+        }
+        else
+            entry++;
+    }
 }
 
 void FirmwareLoader::processRegionList()
--- src/emu/fwloader.h
+++ src/emu/fwloader.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include "emu/map/fw.h"
+#include "emu/fileio.h"
 
 class FirmwareLoader
 {
@@ -19,10 +20,17 @@ protected:
     cfwEntry_t *first(Device &dev);
     cfwEntry_t *next(cfwEntry_t *entry);
 
+    emu::ioFile *processImageFile(fs::path pname, cfwEntry_t *entry);
+    void openImageFile(fs::path pkgName, cfwEntry_t *entry);
+    void closeImageFile();
+    int readImageData(uint8_t *buffer, int length, cfwEntry_t *entry);
+    bool loadImageData(cfwEntry_t *parent, cfwEntry_t *entry);
+
     void processImageEntries(ctag_t *pkgName, cfwEntry_t *&entry, const Device &dev);
     void processRegionList();
 
     Machine &sysMachine;
 
+    emu::ioFile *imageFile = nullptr;
     map::MemoryRegion *region = nullptr;
 };
