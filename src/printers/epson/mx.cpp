e1d6768d328937458c0ad3ac6c56f3786564d086
Timothy Stark <fsword007@gmail.com>
2022-01-09 10:43:55 -0500

    Initial P2 flags. read/write I/O (external data) routines for 8039/8049 microcontroller.

--- src/printers/epson/mx.cpp
+++ src/printers/epson/mx.cpp
@@ -13,25 +13,71 @@
 #include "dev/chip/i8155.h"
 #include "printers/epson/mx.h"
 
+// P1 I/O flag definitions
+
+// P2 I/O flag definitions
+#define P2_IOM      0x20    // 8155 IO/M flag (0 = memory, 1 = I/O)
+#define P2_8155     0x08    // 8155 access enable
+#define P2_8041     0x04    // 8041 access enable
+#define P2_DIP2     0x02    // DIP2 access enable
+#define P2_DIP1     0X01    // DIP1 access enable
+
+// DIP1 switch definitions
+
+// DIP2 switch definitions
+
+
+uint8_t mx80_Device::read8io(offs_t offset)
+{
+    // if ((p2Flags & P2_8155) == 0)
+    // {
+    //     i8155.select(p2Flags & P2_IOM);
+    //     return i8155.read8(offset);
+    // }
+    if ((p2Flags & P2_DIP1) == 0)
+        return sw1Flags;
+    if ((p2Flags & P2_DIP2) == 0)
+        return sw2Flags;
+    return 0;
+}
+
+void mx80_Device::write8io(offs_t offset, uint8_t data)
+{
+    // if ((p2Flags & P2_8155) == 0)
+    // {
+    //     i8155.select(p2Flags & P2_IOM);
+    //     i8155.write8(offset, data);
+    // }
+}
+
 void mx80_Device::mx80(SystemConfig &config)
 {
     I8039(config, pmcu, "pmcu", 0);
     pmcu->setAddressMap(map::asProgram, &mx80_Device::mx80_setProgram);
-    pmcu->setAddressMap(map::asIOPort, &mx80_Device::mx80_setIOPort);
+    pmcu->setAddressMap(map::asIOPort, &mx80_Device::mx80_setExtData);
 
     I8041A(config, smcu, "smcu", 0);
 
+    I8155(config, i8155, "ramio", 0);
+
     // cpu->setAddressMap(map::asProgram, &vt100_Device::vt100_setMemoryMap);
     // cpu->setAddressMap(map::asIOPort, &vt100_Device::vt100_setIOPort);
+
+    sw1Flags = 0;
+    sw2Flags = 0;
 }
 
 void mx80_Device::mx100(SystemConfig &config)
 {
     I8039(config, pmcu, "pmcu", 0);
     I8041A(config, smcu, "smcu", 0);
+    I8155(config, i8155, "ramio", 0);
 
     // cpu->setAddressMap(map::asProgram, &vt100_Device::vt100_setMemoryMap);
     // cpu->setAddressMap(map::asIOPort, &vt100_Device::vt100_setIOPort);
+
+    sw1Flags = 0;
+    sw2Flags = 0;
 }
 
 void mx80_Device::mx80_init()
@@ -51,10 +97,13 @@ void mx80_Device::mx80_setProgram(map::AddressList &map)
     map(0x800, 0xFFF).rom().region("mx80fw", 0x1000);
 }
 
-void mx80_Device::mx80_setIOPort(map::AddressList &map)
+// 8039/8049 external data access
+void mx80_Device::mx80_setExtData(map::AddressList &map)
 {
     map.setUnmappedHigh();
+    // map(0x00, 0xFF).rw(FUNC(mx80_Device::read8io), FUNC(mx80_Device::write8io));
 }
+
 
 static const fwEntry_t FW_NAME(mx80)[] =
 {
--- src/printers/epson/mx.h
+++ src/printers/epson/mx.h
@@ -10,7 +10,7 @@ class mx80_Device : public SystemDevice
 public:
     mx80_Device(const SystemConfig &config, const DeviceType &type, cstag_t &devName, uint64_t clock)
     : SystemDevice(config, type, devName, clock),
-      pmcu(*this, "pmcu"), smcu(*this, "smcu")
+      pmcu(*this, "pmcu"), smcu(*this, "smcu"), i8155(*this, "ramio")
     {
 
     }
@@ -22,9 +22,18 @@ public:
     void mx100_init();
 
     void mx80_setProgram(map::AddressList &map);
-    void mx80_setIOPort(map::AddressList &map);
+    void mx80_setExtData(map::AddressList &map);
 
 private:
-    RequiredDevice<i8039_cpuDevice> pmcu;  // primary microcontroller
-    RequiredDevice<i8041a_cpuDevice> smcu; // slave microcontroller
+    uint8_t read8io(offs_t offset);
+    void write8io(offs_t offset, uint8_t data);
+
+    RequiredDevice<i8039_cpuDevice> pmcu;   // primary microcontroller
+    RequiredDevice<i8041a_cpuDevice> smcu;  // slave microcontroller
+    RequiredDevice<i8155_Device> i8155;     // 8155 RAM, I/O and timer chip
+
+    uint8_t p1Flags;
+    uint8_t p2Flags;
+    uint8_t sw1Flags;
+    uint8_t sw2Flags;
 };
